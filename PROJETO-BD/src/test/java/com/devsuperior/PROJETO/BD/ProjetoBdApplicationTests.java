2b299328a2fe98878bcfcf14efedc83baae0463a
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-11-19 10:42:18 -0300

    Project Created

---
+++ PROJETO-BD/src/test/java/com/devsuperior/PROJETO/BD/ProjetoBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.PROJETO.BD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
